80ac0577bc09f4f535448c3bb4524c9fd2ab948b
Glynis Mullan-Kwok <glynismullankwok@gmail.com>
2020-12-17 16:23:03 -0500

    outline of script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+//create a city list under the enter box with button
+
+
+
+
+//append list/populate weather data conditions, temp, hum, wind and uv
+
+//var queryURL here = city
+
+//call $.ajax here({url:queryURL, method:GET})
+
+//then.(function(weather)){console.log(queryURL)
+//console.log(weatherData)}
+
+//current weather of city and display
