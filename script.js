4db8da2ddfdd76c8a12b52e7240dfd16864e398d
Glynis Mullan-Kwok <glynismullankwok@gmail.com>
2020-12-17 23:39:25 -0500

    script

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+//var cityName =
+
+var apiKey = '71fd9ac4372761cf0c6ce07b04862b3c'
+var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=orlando&appid=' + apiKey
+
+$.ajax({
+    url: queryURL,
+    method: 'GET'
+}).then(function(weatherData) {
+    console.log(weatherData)
+    $('.city').text("City:"+weatherData.name)
+    $('.condition').text("Condition:"+weatherData.condition)
+    $('.wind').text("Wind Speed:"+weatherData.wind)
+    $('.UV').text("UV Index:"+weatherData.uv)
+    $('.humidity').text("Humidity:"+weatherData.humidity)
+    $('.temp').text("Temp:"+((weatherData.main.temp - 273.15) * 9/5 + 32)toFixed() + "F")
+})
+
+
+
 //create a city list under the enter box with button
 
 
